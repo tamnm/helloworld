51789e49eed31fc28e68d2916228969ab3074d66
tamnm <tamnm@minhviettech.com>
2013-06-26 17:15:49 +0700

    Add String Helper
    Signed-off-by: tamnm <tamnm@minhviettech.com>

---
+++ Helloworld/Strings/StringHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Dom;
+using System.Html;
+
+namespace HelloWorld.Strings
+{
+    public static class StringHelper
+    {
+        public static string FormatWith(this string format, params object[] @paramas)
+        {
+            var str = format;
+            for (var i = 0; i < @paramas.length; i++)
+            {
+                str = str.replace("{" + i + "}", @paramas[i].toString());
+            }
+            return str;
+        }
+    }
+}
