aecda75d54b73ac0278ce806b5ecf6c27f8651ab
tamnm <tamnm@minhviettech.com>
2013-06-26 12:13:57 +0700

    Very first commit
    Signed-off-by: tamnm <tamnm@minhviettech.com>

---
+++ HelloWorldSpecs/PersonSpecs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Dom;
+using System.Html;
+using HelloWorld;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloWorldSpecs
+{
+    [TestClass]
+    public class PersonSpecs
+    {
+        [TestMethod]
+        public void Assign_Name()
+        {
+            var p = new Person();
+            p.Name = "Mac Donal";
+
+            Assert.AreEqual("Mac Donal",p.Name);
+        }
+
+        [TestMethod]
+        public void Greeting()
+        {
+            var p = new Person() {Name = "BBQ"};
+            var p2 = new Person() {Name = "KFC"};
+
+            var msg = p.Greeting(p2);
+
+            Assert.AreEqual("Hello KFC", msg);
+        }
+    }
+}
---
+++ Helloworld/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Dom;
+using System.Html;
+using HelloWorld.Strings;
+
+namespace HelloWorld
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Greeting(Person p)
+        {
+            return "Hello {0}".FormatWith(p.Name);
+        }
+    }
+}
